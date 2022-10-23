f8b24cf9e28f51984bd7f5a1a08cc6371be7ec73
kuznetsovkr <kuznetsovkr@yandex.ru>
2022-10-23 14:54:08 +0700

    вёрстка авторизации и регистрации

---
+++ app/src/main/java/com/example/fin_track/registration_activity.java
@@ -0,0 +1,14 @@
+package com.example.fin_track;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registration_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
